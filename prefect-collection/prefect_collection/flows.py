f9c25e48b49a6dfda94b2b95dd6685f82a2d4751
Alexander Streed <alex.s@prefect.io>
2022-02-09 16:18:59 -0600

    Removes test generated project

--- prefect-collection/prefect_collection/flows.py
+++
@@ -1,7 +0,0 @@
-from prefect import flow
-from prefect_collection.tasks import hello_prefect_collection, goodbye_prefect_collection
-
-@flow
-def hello_and_goodbye():
-    print(hello_prefect_collection)
-    print(goodbye_prefect_collection)
--- prefect-collection/tests/test_flows.py
+++
@@ -1,5 +0,0 @@
-from prefect_collection.flows import hello_and_goodbye
-
-def test_hello_and_goodbye_flow():
-    flow_state = hello_and_goodbye()
-    assert flow_state.is_completed
