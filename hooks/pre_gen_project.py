93ffb6f63a2e2fa4d02cb3ee772de96451ca69d9
ahuang11 <andrew.h@prefect.io>
2022-02-22 13:32:38 -0800

    Use raise instead of print + sys.exit

--- hooks/pre_gen_project.py
+++ hooks/pre_gen_project.py
@@ -10,22 +10,19 @@ collection_slug = "{{cookiecutter.collection_slug}}"
 collection_name = "{{cookiecutter.collection_name}}"
 
 if not re.match(MODULE_REGEX, collection_slug):
-    print(
+    raise ValueError(
         f"ERROR: The collection slug ({collection_slug}) is not a valid Python module name. "
         "Please do not use - and use _ instead"
     )
-    sys.exit(1)
 
 if not re.match(PREFECT_COLLECTION_MODULE_REGEX, collection_slug):
-    print(
+    raise ValueError(
         f"ERROR: The collection slug ({collection_slug}) is not a valid Prefect collection module name. "
         "Please ensure your collection slug is prefixed with 'prefect_'"
     )
-    sys.exit(1)
 
 if not re.match(PREFECT_COLLECTION_NAME_REGEX, collection_name):
-    print(
+    raise ValueError(
         "ERROR: The collection name ({collection_name}) is not a valid Prefect collection name. "
         "Please ensure your collection slug is prefixed with 'prefect-'"
     )
-    sys.exit(1)
