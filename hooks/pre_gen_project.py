5262b3350c9d7abe7a4f4e22742887938e712b97
Alexander Streed <alex.s@prefect.io>
2022-02-22 14:58:21 -0600

    Adds pre_gen hook to verify collection name and slug

---
+++ hooks/pre_gen_project.py
@@ -0,0 +1,31 @@
+import re
+import sys
+
+
+MODULE_REGEX = r"^[_a-zA-Z][_a-zA-Z0-9]+$"
+PREFECT_COLLECTION_MODULE_REGEX = r"^prefect_[_a-zA-Z][_a-zA-Z0-9]+$"
+PREFECT_COLLECTION_NAME_REGEX = r"^prefect-[-a-zA-Z][-a-zA-Z0-9]+$"
+
+collection_slug = "{{cookiecutter.collection_slug}}"
+collection_name = "{{cookiecutter.collection_name}}"
+
+if not re.match(MODULE_REGEX, collection_slug):
+    print(
+        f"ERROR: The collection slug ({collection_slug}) is not a valid Python module name. "
+        "Please do not use - and use _ instead"
+    )
+    sys.exit(1)
+
+if not re.match(PREFECT_COLLECTION_MODULE_REGEX, collection_slug):
+    print(
+        f"ERROR: The collection slug ({collection_slug}) is not a valid Prefect collection module name. "
+        "Please ensure your collection slug is prefixed with 'prefect_'"
+    )
+    sys.exit(1)
+
+if not re.match(PREFECT_COLLECTION_NAME_REGEX, collection_name):
+    print(
+        "ERROR: The collection name ({collection_name}) is not a valid Prefect collection name. "
+        "Please ensure your collection slug is prefixed with 'prefect-'"
+    )
+    sys.exit(1)
