3ab77addf26bd2a8ff7fe1b6ed10bda0b77b18d5
Alexander Streed <alex.s@prefect.io>
2022-04-04 15:37:02 -0500

    Adds spaces

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -1,5 +1,5 @@
 print(
-    "Your Prefect Collection has been generated! Please refer"
-    "to MAINTAINERS.md in the generated project for the next steps"
+    "Your Prefect Collection has been generated! Please refer "
+    "to MAINTAINERS.md in the generated project for the next steps "
     "developing your Prefect Collection"
 )
