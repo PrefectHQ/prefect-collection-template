a9f1a6ed131defbee1abbfbda1f3c076abe60a1e
ahuang11 <andrew.h@prefect.io>
2022-08-16 15:09:32 -0700

    Dont instantiate clas

--- {{cookiecutter.collection_name}}/{{cookiecutter.collection_slug}}/flows.py
+++ {{cookiecutter.collection_name}}/{{cookiecutter.collection_slug}}/flows.py
@@ -13,7 +13,7 @@ def hello_and_goodbye():
     """
     Sample flow that says hello and goodbye!
     """
-    {{ cookiecutter.collection_name.split('-')[1:] | join | title}}Block().seed_value_for_example()
+    {{ cookiecutter.collection_name.split('-')[1:] | join | title}}Block.seed_value_for_example()
     block = {{ cookiecutter.collection_name.split('-')[1:] | join | title}}Block.load("sample-block")
 
     print(hello_{{ cookiecutter.collection_slug }}())
