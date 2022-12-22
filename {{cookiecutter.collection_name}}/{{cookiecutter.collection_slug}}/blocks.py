ce14fb784b8ac201049ee44dbe4dd096f3de290d
Andrew <15331990+ahuang11@users.noreply.github.com>
2022-12-22 13:45:35 -0800

    Update blocks.py

--- {{cookiecutter.collection_name}}/{{cookiecutter.collection_slug}}/blocks.py
+++ {{cookiecutter.collection_name}}/{{cookiecutter.collection_slug}}/blocks.py
@@ -22,7 +22,7 @@ class {{ cookiecutter.collection_name.split('-')[1:] | join | title}}Block(Block
     _block_type_name = "{{ cookiecutter.collection_name.split('-')[1:] | join(' ') }}"
     # _logo_url = "https://path/to/logo.png"
 
-    value: str = Field("The default value", description="The value to store")
+    value: str = Field("The default value", description="The value to store.")
 
     @classmethod
     def seed_value_for_example(cls):
