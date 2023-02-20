445b9aafbc601acfddf2086092aaabe0894fe37e
Alexander Streed <alex.s@prefect.io>
2023-02-20 09:11:13 -0600

    Fixes formatting errors

--- {{cookiecutter.collection_name}}/docs/gen_blocks_catalog.py
+++ {{cookiecutter.collection_name}}/docs/gen_blocks_catalog.py
@@ -9,8 +9,7 @@ from textwrap import dedent
 import mkdocs_gen_files
 from prefect.blocks.core import Block
 from prefect.utilities.dispatch import get_registry_for_type
-from prefect.utilities.importtools import (from_qualified_name,
-                                           to_qualified_name)
+from prefect.utilities.importtools import from_qualified_name, to_qualified_name
 
 COLLECTION_SLUG = "{{ cookiecutter.collection_slug }}"
 
